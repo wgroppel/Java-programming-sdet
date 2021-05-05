a1774f936bbfc4b1ebe2479236b216f92e5dc61b
William Groppel <williamgroppel7@gmail.com>
2021-05-04 19:07:35 -0500

    Changes to new Class

--- src/day42_arrayList/CollectionsUtil.java
+++ src/day42_arrayList/CollectionsUtil.java
@@ -1,5 +1,6 @@
 package day42_arrayList;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class CollectionsUtil {
@@ -13,6 +14,45 @@ public class CollectionsUtil {
         System.out.println("reversed = " + letters);
 
         System.out.println(Collections.frequency(letters, 'a'));
+
+        int vCount = Collections.frequency(letters, 'v');
+        System.out.println("vCount = " + vCount);
+
+        System.out.println("Max Char = " + Collections.max(letters));
+        System.out.println("Min Char = " + Collections.min(letters));
+
+        Collections.reverse(letters);
+        Collections.replaceAll(letters, 'a', 'u');
+        Collections.replaceAll(letters, 'i', 'j');
+        System.out.println("After replaceAll = " + letters);
+
+        Collections.sort(letters);
+        System.out.println("After sort = " + letters);
+
+        List<Integer> nums = Arrays.asList(23, 4, 654, 32, 65, 32, -8, 1);
+        System.out.println("nums = " + nums);
+
+        Collections.reverse(nums);
+        System.out.println("nums after reverse = " + nums);
+
+        int max = Collections.max(nums);
+        int min = Collections.min(nums);
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+        int countOfFives = Collections.frequency(nums, 32);
+        System.out.println("countOfFives = " + countOfFives);
+
+        int countOfOnes = Collections.frequency(nums, 1);
+        System.out.println("countOfOnes = " + countOfOnes);
+
+        Collections.replaceAll(nums, 1, 99);
+        System.out.println("after replaceAll = " + nums);
+
+        Collections.sort(nums, Collections.reverseOrder());
+        System.out.println("after revers sort = " + nums);
+
+
 
     }
 }
